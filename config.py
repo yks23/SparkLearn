0c80e87429770207f8261ee23c5e98492bf0172c
YangKaisen <yks23@mails.tsinghua.edu.cn>
2025-07-21 01:21:04 +0800

    update to dsv3

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
-model_name = "4.0Ultra"
+# model_name = "4.0Ultra"
+model_name = "Pro/deepseek-ai/DeepSeek-V3"
+
 """
 4.0Ultra
 generalv3.5
@@ -7,17 +9,35 @@ generalv3
 pro-128k
 lite
 """
-max_thread_num = 4
+max_thread_num = 8
 """ Maximum number of threads for parallel processing """
-extra_models = ["generalv3.5", "max-32k", "generalv3", "pro-128k", "lite"]
+# extra_models = ["generalv3.5", "max-32k", "generalv3", "pro-128k", "lite"]
+
+extra_models = [
+    "Pro/THUDM/glm-4-9b-chat",
+    "THUDM/glm-4-9b-chat",
+    "deepseek-ai/DeepSeek-V2.5",
+    "deepseek-ai/DeepSeek-V3",
+    "Pro/deepseek-ai/DeepSeek-V3",
+]
+
+
 """ List of additional models to use, e.g., ['4.0Ultra', 'generalv3.5'] """
+# json_feature = {
+#     "4.0Ultra": True,
+#     "generalv3.5": True,
+#     "max-32k": True,
+#     "generalv3": True,
+#     "pro-128k": True,
+#     "lite": True,
+# }
+# for siliconflow
 json_feature = {
-    "4.0Ultra": True,
-    "generalv3.5": True,
-    "max-32k": True,
-    "generalv3": True,
-    "pro-128k": True,
-    "lite": True,
+    "THUDM/glm-4-9b-chat": True,
+    "Pro/THUDM/glm-4-9b-chat": True,
+    "deepseek-ai/DeepSeek-V2.5": True,
+    "deepseek-ai/DeepSeek-V3": True,
+    "Pro/deepseek-ai/DeepSeek-V3": False,
 }
 # API key configs
 spark_api_key = "xqZXJdxoaaLEBIMuuCBg:FUuIHcWDmPLSpJcXsiSp"
